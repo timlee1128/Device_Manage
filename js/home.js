8720514464ae9fd71693b64900b78486cd5c7420
Tim <490003773@qq.com>
2017-05-18 18:20:02 +0800

    Complete Home page and popup component

---
+++ js/home.js
@@ -0,0 +1,29 @@
+$(function() {
+  // Dynamic style
+  resize();
+  window.onresize = function() {
+    resize();
+  }
+  function resize() {
+    var viewport = getViewportSize();
+    var mapRatio = {
+      w: viewport.w / 1920,
+      h: viewport.h / 1080
+    };
+
+    var rem = 100 * mapRatio.h;
+    $('#bg, #pop_up').css({
+      "width": viewport.w + 'px',
+      "height": viewport.h + 'px'
+    });
+    document.documentElement.style.fontSize = rem + 'px';
+  }
+
+  // Normal
+  $('#shift_zone').click(function() {
+    $('#pop_up').show();
+  });
+  $('#pop_up_shade, #pop_up_content i').click(function() {
+    $('#pop_up').hide();
+  })
+});
